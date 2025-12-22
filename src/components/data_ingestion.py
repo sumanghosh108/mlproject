1a5b34e976a1e1afa1ad65e1d0c449cdadff0f53
Suman Ghosh <sumanghosh8767@gmail.com>
2025-12-22 23:53:12 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -51,3 +53,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error,error_detail:sys):
+def error_message_detail(error,error_detail):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message='Error occured in python script name [{0}] line number [{1}] error message [{2}]'.format(
@@ -9,7 +9,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_detail):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
